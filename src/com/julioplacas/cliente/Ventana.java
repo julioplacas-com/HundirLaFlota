2c13b58b5d446314f78a49ace16b844620f685a4
Adrinilo <93315833+Adrinilo@users.noreply.github.com>
2023-03-14 12:55:21 +0100

    eventos_cliente

--- src/com/julioplacas/cliente/Ventana.java
+++ src/com/julioplacas/cliente/Ventana.java
@@ -20,64 +20,76 @@ import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 
 import com.julioplacas.modelo.Barco;
+import com.julioplacas.modelo.Direccion;
 import com.julioplacas.modelo.Estado;
 
-public class Ventana extends JFrame implements ActionListener {
+public class Ventana extends JFrame implements Runnable, ActionListener {
 	private static final int BOARD_SIZE = 10;
-	private JButton[][] buttons;
-	private int[][] board;
-	private boolean activo = true;
-	private int barc = 2;
-	
+	private JButton[][] mis_barcos;
+	private JButton[][] sus_barcos;
+
 	private final Socket socket;
 	private final ObjectOutputStream fSalida;
 	private final ObjectInputStream fEntrada;
 	private final Barco[] barcos;
-	
+
 	private int turno;
 	private Estado estado;
 
-	public Ventana(
-			Socket socket,
-			ObjectOutputStream fSalida,
-			ObjectInputStream fEntrada,
-			Barco[] barcos
-	) throws IOException {
+	private Thread hilo;
+
+	public Ventana(Socket socket, ObjectOutputStream fSalida, ObjectInputStream fEntrada, Barco[] barcos)
+			throws IOException {
 		super("Battleship");
-		
+
 		this.socket = socket;
 		this.fSalida = fSalida;
 		this.fEntrada = fEntrada;
 		this.barcos = barcos;
 
+		hilo = new Thread(this);
+		hilo.start();
+
 		turno = fEntrada.readInt();
 		System.out.println("Turno: " + turno);
 		estado = Estado.values()[fEntrada.readInt()];
 		System.out.println("Estado: " + estado);
-		
+
 		setExtendedState(MAXIMIZED_BOTH);
 		setResizable(false);
-		buttons = new JButton[BOARD_SIZE][BOARD_SIZE];
-		board = new int[BOARD_SIZE][BOARD_SIZE];
-		initBoard();
 		setLayout(new GridLayout(1, 2));
 		Container pane = this.getContentPane();
-		initButtons(pane, BorderLayout.LINE_START, false);
-		initButtons(pane, BorderLayout.LINE_END, true);
+		mis_barcos = initButtons(pane, BorderLayout.LINE_START, false);
+		sus_barcos = initButtons(pane, BorderLayout.LINE_END, true);
 		setVisible(true);
 	}
 
-	// Inicializa la matriz board con casillas vacías
-	private void initBoard() {
-		for (int i = 0; i < BOARD_SIZE; i++) {
-			for (int j = 0; j < BOARD_SIZE; j++) {
-				board[i][j] = 0; // casilla vacía
+	public void run() {
+		while (true) {
+			if (turno == 1 && estado == Estado.TURNO_JUGADOR_1) {
+				try {
+					hilo.wait();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			} else {
+				try {
+					estado = Estado.values()[fEntrada.readInt()];
+					int x = fEntrada.readInt();
+					int y = fEntrada.readInt();
+					sus_barcos[x][y].setBackground(Color.RED);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
 		}
 	}
 
 	// Crea los botones y los añade al JFrame
-	private void initButtons(Container pane, String pos, boolean accion) {
+	private JButton[][] initButtons(Container pane, String pos, boolean accion) {
+		JButton[][] buttons = new JButton[BOARD_SIZE][BOARD_SIZE];
 		JPanel frame = new JPanel();
 		pane.add(frame, pos);
 
@@ -101,35 +113,52 @@ public class Ventana extends JFrame implements ActionListener {
 				frame.add(buttons[i][j]);
 			}
 		}
+		return buttons;
 	}
 
 	// Lógica del juego cuando se hace clic en un botón
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		JButton button = (JButton) e.getSource();
-		int row = -1, col = -1;
+		int x = -1, y = -1;
 		for (int i = 0; i < BOARD_SIZE; i++) {
 			for (int j = 0; j < BOARD_SIZE; j++) {
-				if (buttons[i][j] == button) {
-					row = i;
-					col = j;
+				if (sus_barcos[i][j] == button) {
+					x = i;
+					y = j;
 					break;
 				}
 			}
 		}
-
-		if (board[row][col] == 1) {
-			button.setBackground(Color.RED); // barco hundido
-			board[row][col] = 2; // marcar como atacada
+		sus_barcos[x][y].setEnabled(false);
+		if (hayBarco(x, y)) {
+			sus_barcos[x][y].setBackground(Color.YELLOW);
 		} else {
-			button.setBackground(Color.GRAY); // casilla vacía
-			board[row][col] = 2;
-			activo = false;
-			button.setEnabled(activo); // marcar como atacada
-		}
-		if (board[row][col] == 1) {
-			JOptionPane.showMessageDialog(this, "Barco encontrado en la posición " + (char) ('A' + col) + (row + 1));
+			sus_barcos[x][y].setBackground(Color.BLUE);
 		}
 	}
 
+	public boolean hayBarco(int x, int y) {
+		for (Barco barco : barcos) {
+			if (barcoEnHorizontal(x, y, barco) || barcoEnVertical(x, y, barco))
+				return true;
+		}
+		return false;
+	}
+
+	private boolean barcoEnHorizontal(int x, int y, Barco barco) {
+		return
+				barco.direccion == Direccion.HORIZONTAL
+				&& barco.posicion.y == y
+				&& barco.posicion.x >= x
+				&& barco.posicion.x + barco.longitud <= barco.posicion.x;
+	}
+
+	private boolean barcoEnVertical(int x, int y, Barco barco) {
+		return
+				barco.direccion == Direccion.VERTICAL
+				&& barco.posicion.x == x
+				&& barco.posicion.y >= y
+				&& barco.posicion.y + barco.longitud <= barco.posicion.y;
+	}
 }
