fef2916d6e6f728ce53127c48f9df0781d8c27e1
aruZeta <me@aruzeta.com>
2023-03-16 12:57:29 +0100

    [add] Change subdomain

--- src/com/julioplacas/utilidad/Utilidad.java
+++ src/com/julioplacas/utilidad/Utilidad.java
@@ -5,7 +5,7 @@ import com.julioplacas.modelo.Direccion;
 
 public final class Utilidad {
   public static final int PUERTO = 3000;
-  public static final String SERVER = "psp.julioplacas.com";
+  public static final String SERVER = "nube2.julioplacas.com";
   public static final int MAX_SALAS = 5;
   public static final int MAX_SIZE = 10;
 
